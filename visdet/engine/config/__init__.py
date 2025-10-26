74de11fefe68b70c91eac69bcdd33cc91645634a
George Pearse <georgehwp26@gmail.com>
2025-10-26 20:59:20 +0000

    Export load_yaml_config from config module
    Make load_yaml_config available from visdet.engine.config for preset loading.

--- visdet/engine/config/__init__.py
+++ visdet/engine/config/__init__.py
@@ -2,5 +2,6 @@
 # type: ignore
 # Copyright (c) OpenMMLab. All rights reserved.
 from .config import Config, ConfigDict, DictAction, read_base
+from .yaml_loader import load_yaml_config
 
-__all__ = ["Config", "ConfigDict", "DictAction", "read_base"]
+__all__ = ["Config", "ConfigDict", "DictAction", "read_base", "load_yaml_config"]
